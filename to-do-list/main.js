823ce91544a480fa8f5a11ad8c06dc7cf1f00ee1
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-01-30 21:22:23 +0900

    기능별 함수 분리

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,72 +1,75 @@
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
-let textarea = document.getElementById("toDoContent");
+let toDoContent = document.getElementById("toDoContent");
 let contentList = document.getElementById("contentList");
+let localData = []
 
 form.addEventListener("submit", (e) => { // id"form" 내부 다 가져옴.
-    e.preventDefault();
-    formValidation();
-  });
-  // 유효성검사 가볍게 하기
-let formValidation = () => { 
-    textInput.value === "" || textarea.value === "" ?
-    contentMsg.innerHTML = "제목 또는 내용 없음" : dataInLocal();
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {  // 내용 없으면 알림.
+  textInput.value === "" || toDoContent.value === "" ?
+  contentMsg.innerHTML = "제목 또는 내용 없음" : dataInLocal();
 };
 
+let resetForm = () => { // 작성 후 입력 비우기
+  textInput.value = "";
+  toDoContent.value = "";
+  contentMsg.innerHTML ="";
+};
 
-let localData = [];
-let dataInLocal = () => { // 로컬에 데이터 넣기
-  localData.push({
+let localMap = () => {  // 호출 한 함수에 따라 맵.
+  contentList.innerHTML = "";
+    localData.map((el, index) => {
+      return (contentList.innerHTML += `
+        <div id="${index}">
+          <p>${index+1}.제목 : ${el.text}</p>
+          <span>${el.date}</span>
+          <p>${el.description}</p>
+          <button type="submit" id="delete" onclick="deleteContentList(this)">삭제하기</button>
+        </div>
+        `
+)})};
+
+let localSetData = () =>{ // 로컬에 데이터 넣기
+  localStorage.setItem("localDataKey", JSON.stringify(localData));
+}
+
+let dataInLocal = () => { // data넣기
+  localData.unshift({
     text: textInput.value,
     date: new Date().toLocaleString('kr'),
-    description: textarea.value,
+    description: toDoContent.value,
   });
-  localStorage.setItem("localDataKey", JSON.stringify(localData)); // key, value
+  localSetData();
   createContentList();
-  console.log("localData : ",localData);
 };
-let createContentList = () => { // 로컬데이터 html 넣기
-  contentList.innerHTML = "";
-  localData.map((el, index) => { // el, index가 사용되는건 배열의 구조상 필요함. 규칙임
-      return (contentList.innerHTML += `
-              <div id="${index}">
-                <p>${index+1}.제목 : ${el.text}</p>
-                <span>${el.date}</span>
-                <p>${el.description}</p>
-                <button type="submit" id="delete" onclick="deleteContentList(this)">삭제하기</button>
-              </div>
-            `
-            );
-          });
-    resetForm();
-  };
-  let resetForm = () => {
-    textInput.value = "";
-    textarea.value = "";
-    contentMsg.innerHTML ="";
-  };
 
-  // 최상단이 삭제됨. -> 삭제대상 태그에 id값이 없으면 최상단이 디폴트인듯?
-  // 모두 삭제해도 로컬상 몇몇 데이터 남아있음.
-  // 나중에 추가 한 데이터부터 지우면 다 지워짐.
-  let deleteContentList = (e) => {
-    e.parentElement.remove();
-    localData.splice(e.parentElement.id, 1);  // 스플라이스로 지우고, 
-    localStorage.setItem("localDataKey", JSON.stringify(localData));  // 로컬에 스트링으로 셋.
-    createContentList();
-  };
-  
-  let delAllLocalStorage = () => {
-    localData.splice(0, localData.length);
-    localStorage.setItem("localDataKey", JSON.stringify(localData));
-    createContentList();
-  }
-// 로컬 가져옴.
-  (() => {
-    localData = JSON.parse(localStorage.getItem("localDataKey")) || []; // json형식으로 겟.
-    createContentList();
-  })();
+let deleteContentList = (e) => {  // 삭제하기
+  e.parentElement.remove();
+  localData.splice(e.parentElement.id, 1);  
+  localSetData();
+  localMap();
+};
 
-  // 추후 리액트 적용, 다른기능 적용
+let delAllLocalStorage = () => {  // 전체삭제
+  localData.splice(0, localData.length);
+  localSetData();
+  localMap();
+}
+
+let createContentList = () => { // 추가하기
+  localMap();
+  resetForm();
+};
+
+localData = JSON.parse(localStorage.getItem("localDataKey")) || []; // 바로 return
+localMap(); // 필드실행.
+
+
+  // 추가 할 기능 생각해보기
+  // 함수 기능별로 쪼개기
 
 
