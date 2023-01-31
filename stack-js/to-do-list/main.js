bffd891bc9359c2bb3f0a18db30a8708dd5269d9
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-01-31 18:37:43 +0900

    가독성 향상

--- stack-js/to-do-list/main.js
+++ stack-js/to-do-list/main.js
@@ -3,73 +3,63 @@ let titleInput = document.getElementById("textInput");
 let toDoContent = document.getElementById("toDoContent");
 let contentList = document.getElementById("contentList");
 let localData = JSON.parse(localStorage.getItem("localDataKey")) || [];
-
 /** 유효성검사 */
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  titleInput.value === "" || toDoContent.value === "" ?
-  contentMsg.innerHTML = "제목 또는 내용 없음" : dataInLocal();
-});
-
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      titleInput.value === "" || toDoContent.value === "" ?
+      contentMsg.innerHTML = "제목 또는 내용 없음" : dataInLocalData();
+    });
 /** 입력값 초기화 */
-let resetForm = () => { 
-  titleInput.value = "";
-  toDoContent.value = "";
-  contentMsg.innerHTML ="";
-};
-
-let localMap = () => {
-  contentList.innerHTML = "";
-    localData.map((el, index) => {
-      return (contentList.innerHTML += `
-        <div id="${index}">
-          <p>${index+1}.제목 : ${el.title}</p>
-          <span>${el.date}</span>
-          <p>${el.content}</p>
-          <button type="submit" id="delete" onclick="deleteContentList(this)">삭제하기</button>
-        </div>
-        `
-)})};
-
-/** 함수실행 */
-let callCenter = (x, y) => {
-  x(), y();
-}
-
+    let resetForm = () => {
+      titleInput.value = "";
+      toDoContent.value = "";
+      contentMsg.innerHTML ="";
+    };
+/** 리스트 불러오기 */
+    let localMap = () => {
+      contentList.innerHTML = "";
+        localData.map((el, index) => {
+          return (contentList.innerHTML += `
+            <div id="${index}">
+              <p>${index+1}.제목 : ${el.title}</p>
+              <span>${el.date}</span>
+              <p>${el.content}</p>
+              <button type="submit" id="delete" onclick="deleteContentList(this)">삭제하기</button>
+            </div>
+            `
+    )})};
 /** 로컬에 데이터 넣기 */
-let localSetData = () =>{
-  localStorage.setItem("localDataKey", JSON.stringify(localData));
-}
-
+    let localDataSet = () =>{
+      localStorage.setItem("localDataKey", JSON.stringify(localData));
+    }
 /** 데이터 넣기 */
-let dataInLocal = () => {
-  localData.unshift({
-    title: titleInput.value,
-    date: new Date().toLocaleString('kr'),
-    content: toDoContent.value,
-  });
-  callCenter(localSetData, createContentList);
-};
-
-/** 해당 게시글 삭제 */
-let deleteContentList = (e) => {
-  e.parentElement.remove();
-  localData.splice(e.parentElement.id, 1);  
-  callCenter(localSetData, localMap);
-};
-
-/** 게시글 전체삭제 */
-let delAllLocalStorage = () => {  
-  localData.splice(0, localData.length);
-  callCenter(localSetData, localMap);
-}
-
+    let dataInLocalData = () => {
+      localData.unshift({
+        title: titleInput.value,
+        date: new Date().toLocaleString('kr'),
+        content: toDoContent.value,
+      });
+      callCenter(localDataSet, createContentList);
+    };
 /** 리스트에 추가하기 */ 
-let createContentList = () => { 
-  callCenter(localMap, resetForm);
-};
-
-localMap(); // 필드실행.
+    let createContentList = () => { 
+      callCenter(localMap, resetForm);
+    };
+/** 해당 게시글 삭제 */
+    let deleteContentList = (e) => {
+      e.parentElement.remove();
+      localData.splice(e.parentElement.id, 1);  
+      callCenter(localDataSet, localMap);
+    };
+/** 게시글 전체삭제 */
+    let delAllLocalStorage = () => {  
+      localData.splice(0, localData.length);
+      callCenter(localDataSet, localMap);
+    }
+/** 함수실행 */
+    let callCenter = (func1, func2) => {func1(), func2()}
+// 필드실행.
+localMap(); 
 
   // 추가 할 기능 생각해보기
   // 함수 기능별로 쪼개기
