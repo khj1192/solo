9aa2313dd6e0feba33dcf3b1af30d0731f92d3ed
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-02-01 16:52:22 +0900

    비효율적인 필터링

--- stack-js/search-and-filters/app.js
+++ stack-js/search-and-filters/app.js
@@ -1,2 +1,119 @@
 let dummyTest = dummy;
-console.log(dummyTest);
+console.log([...dummyTest]);
+let dummyList = document.querySelector(".dummy-list");
+
+// 일단 리스트 뿌림.
+dummyList.innerHTML = dummyTest.map((el) => {
+    const {id, name, imgUrl, techStack} = el;  // el.id, el.name
+    return (
+        `
+        <div>
+        <img src="${imgUrl}" />
+        <span>아이디:${id}</span>
+        <span>이름:${name}</span><br>
+        <span>기술스택:${techStack}</span>
+        </div>
+        `
+    )
+}).join('');
+
+{/* <article class="categories">
+<button class="btn">전체</button>
+<button class="btn">프론트</button>
+<button class="btn">백</button>
+<button class="btn">풀스택</button>
+</article> */}
+
+let btn = document.querySelectorAll(".btn");
+let listOut = () => {
+    // (btn[0].value === 'all') 전체출력()
+    // (btn[0].value === 'all') 전체출력()
+    // (btn[0].value === 'all') 전체출력()
+    // (btn[0].value === 'all') 전체출력()
+}
+btn[0].addEventListener("click", () =>{
+    dummyList.innerHTML = dummyTest.map(el =>{
+        const {id, name, imgUrl, techStack} = el;
+        return (
+            `
+            <div>
+            <img src="${imgUrl}" />
+            <span>아이디:${id}</span>
+            <span>이름:${name}</span><br>
+            <span>기술스택:${techStack}</span>
+            </div>
+            `
+        )
+    });
+});
+btn[1].addEventListener("click", () =>{
+    dummyList.innerHTML = dummyTest.filter((el) => {
+        if(el.tech === 'front'){ return el }
+    }).map(el =>{
+        const {id, name, imgUrl, techStack} = el;
+        return (
+            `
+            <div>
+            <img src="${imgUrl}" />
+            <span>아이디:${id}</span>
+            <span>이름:${name}</span><br>
+            <span>기술스택:${techStack}</span>
+            </div>
+            `
+        )
+    });
+});
+btn[2].addEventListener("click", () =>{
+    dummyList.innerHTML = dummyTest.filter((el) => {
+        if(el.tech === 'back'){ return el }
+    }).map(el =>{
+        const {id, name, imgUrl, techStack} = el;
+        return (
+            `
+            <div>
+            <img src="${imgUrl}" />
+            <span>아이디:${id}</span>
+            <span>이름:${name}</span><br>
+            <span>기술스택:${techStack}</span>
+            </div>
+            `
+        )
+    });
+});
+btn[3].addEventListener("click", () =>{
+    dummyList.innerHTML = dummyTest.filter((el) => {
+        if(el.tech === 'full'){ return el }
+    }).map(el =>{
+        const {id, name, imgUrl, techStack} = el;
+        return (
+            `
+            <div>
+            <img src="${imgUrl}" />
+            <span>아이디:${id}</span>
+            <span>이름:${name}</span><br>
+            <span>기술스택:${techStack}</span>
+            </div>
+            `
+        )
+    });
+});
+
+
+
+// if(tech === 'front')
+// if(tech === 'back')
+// return (
+//     `
+//     <div>
+//     <img src="${imgUrl}" />
+//     <span>아이디:${id}</span>
+//     <span>이름:${name}</span><br>
+//     <span>기술스택:${techStack}</span>
+//     </div>
+//     `
+// )
+
+
+
+
+
--- stack-js/search-and-filters/dummy.js
+++ stack-js/search-and-filters/dummy.js
@@ -1,17 +1,39 @@
 const dummy = [
     {
-        id : "0"
+        id : "0",
+        name : "HJ0",
+        imgUrl :"https://avatars.githubusercontent.com/u/1",
+        techStack : "java, javascript, typescript",
+        tech : "full"
     },
     {
-        id : "1"
+        id : "1",
+        name : "HJ1",
+        imgUrl :"https://avatars.githubusercontent.com/u/2",
+        techStack : "java, javascript, typescript",
+        tech : "full"
     },
     {
-        id : "2"
+        id : "2",
+        name : "HJ2",
+        imgUrl :"https://avatars.githubusercontent.com/u/3",
+        techStack : "java, javascript, typescript",
+        tech : "full"
     },
     {
-        id : "3"
+        id : "3",
+        name : "HJ3",
+        imgUrl :"https://avatars.githubusercontent.com/u/4",
+        techStack : "java, javascript, typescript",
+        tech : "front"
     },
     {
-        id : "4"
+        id : "4",
+        name : "HJ4",
+        imgUrl :"https://avatars.githubusercontent.com/u/5",
+        techStack : "java, javascript, typescript",
+        tech : "back"
     }
 ]
+// 메인1, 서브0~3, 기타 0~5
+// 프론트, 백, 풀
