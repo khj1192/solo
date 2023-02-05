6cdddbefad18207f860a5a8b94a0aa66408b1a90
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-02-05 16:49:03 +0900

    체크값 등록

--- stack-js/search-and-filters/app.js
+++ stack-js/search-and-filters/app.js
@@ -4,63 +4,57 @@ let textName = document.getElementById("textName");
 let skillStack = document.getElementById("skillStack");
 let localData = JSON.parse(localStorage.getItem("developerGroup")) || [];
 // 초기 더미 로컬에 등록
-const testReturn = (() => {
+const addLocalStorage = (() => {
     if(!localStorage.developerGroup) {
         localStorage.setItem('developerGroup', JSON.stringify(dummy));
     }
 })()
-// 수정필요...
-// checkBox.addEventListener("checkbox", () => {
-//     console.log("체크박스실행여부")
-// })
 
-// 등록버튼 클릭시 실행
 let addDeveloper = () => {
+    let tech = "";
+    if (front.checked) tech += front.value
+    if (back.checked) tech += back.value
+    if (full.checked) tech += full.value
+    console.log("tech : ",tech)
     localData.push({
         id: localData.length + 1,
         name: textName.value, 
         imgUrl: `https://avatars.githubusercontent.com/u/${parseInt(Math.random() * 100)}`,
         techStack: skillStack.value,
-        tech: "front"
+        tech: tech,
     });
     localStorage.setItem("developerGroup", JSON.stringify(localData));
 };
 
+// 등록버튼 클릭시 실행
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     addDeveloper();
 });
 
+/** 인자가 전달되지 않으면 'all'할당 -> filter조건 프리패스권. */
 const displayData = (tech = 'all') => {
     dummyList.innerHTML = localData
-      .filter((el) => tech === 'all' || el.tech === tech)
-      .map(el => `
+    .filter((el) => tech === 'all' || el.tech === tech)
+    .map(el => `
         <div>
-            <img src="${el.imgUrl}" />
-            <span>ID: ${el.id}</span>
-            <span>Name: ${el.name}</span>
-            <span>Tech Stack: ${el.techStack}</span>
+        <img src="${el.imgUrl}" />
+        <span>ID: ${el.id}</span>
+        <span>Name: ${el.name}</span>
+        <span>Tech Stack: ${el.techStack}</span>
         </div>
-      `)
-      .join('');
+        `)
+    .join('');
 };
 displayData();
 
-// 필터링.
-// let btn = document.querySelectorAll(".btn");
-//   btn[0].addEventListener("click", () => displayData('all'));
-//   btn[1].addEventListener("click", () => displayData('front'));
-//   btn[2].addEventListener("click", () => displayData('back'));
-//   btn[3].addEventListener("click", () => displayData('full'));
-
-const buttons = document.querySelectorAll(".btn");
-    buttons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        console.log("실행여부확인")
-        let tech = ['all', 'front', 'back', 'full'][index];
+// 필터클릭시 디스플레이함수에 전달.
+const btn = document.querySelector(".categories");
+    btn.addEventListener("click", (e)=>{
+        let tech = e.target.value;
+        console.log(tech)
         displayData(tech);
     });
-});
 
 const formInput = document.querySelector('.input-form');
 const searchInput = document.querySelector(".search");
@@ -71,6 +65,7 @@ formInput.addEventListener('keyup', () => {
 });
 
   // 구조정렬하기
+
 
 
 
