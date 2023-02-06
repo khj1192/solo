0c27ca1a045ba59f21273d8687af022763e92585
SEB_FE_43_김현진 <sossos1191@gmail.com>
2023-02-06 23:07:06 +0900

    페이지처리

--- stack-js/search-and-filters/app.js
+++ stack-js/search-and-filters/app.js
@@ -33,7 +33,7 @@ form.addEventListener("submit", (e) => {
 });
 
 /** 인자가 전달되지 않으면 'all'할당 -> filter조건 프리패스권. */
-const displayData = (tech = 'all') => {
+let displayData = (tech = 'all') => {
     dummyList.innerHTML = localData
     .filter((el) => tech === 'all' || el.tech === tech)
     .map(el => `
@@ -63,6 +63,58 @@ formInput.addEventListener('keyup', () => {
     localData = localData.filter(el => el.techStack.toLowerCase().includes(inputValue));
     displayData();
 });
+
+////////////////////페이징//////////////////////
+// 문제1. 버튼 눌러야 작동
+// 문제2. 어느 카테고리로 가도 전체 페이지 수 가져옴.
+const itemsPerPage = 5;
+let currentPage = 1;
+
+const pagination = () => {
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const pagedData = localData.slice(startIndex, endIndex);
+  dummyList.innerHTML = pagedData
+    .map(el => `
+        <div>
+        <img src="${el.imgUrl}" />
+        <span>ID: ${el.id}</span>
+        <span>Name: ${el.name}</span>
+        <span>Tech Stack: ${el.techStack}</span>
+        </div>
+        `)
+    .join('');
+};
+
+const changePage = (page) => {
+  currentPage = page;
+  pagination();
+};
+
+const createPaginationButtons = () => {
+    const pagesCount = Math.ceil(JSON.parse(localStorage.getItem("developerGroup")).length / itemsPerPage);
+    let buttons = "";
+    for (let i = 1; i <= pagesCount; i++) {
+      buttons += `<button value="${i}" onClick="changePage(${i})">${i}</button>`;
+    }
+    document.querySelector(".pagination").innerHTML = buttons;
+  };
+
+displayData = (tech = 'all') => {
+  localData = JSON.parse(localStorage.getItem("developerGroup"))
+    .filter((el) => tech === 'all' || el.tech === tech);
+  createPaginationButtons();
+  pagination();
+};
+
+formInput.addEventListener('keyup', () => {
+  const inputValue = searchInput.value.toLowerCase();
+  localData = JSON.parse(localStorage.getItem("developerGroup"))
+    .filter((el) => el.techStack.toLowerCase().includes(inputValue));
+  createPaginationButtons();
+  pagination();
+});
+
 
   // 구조정렬하기
 
